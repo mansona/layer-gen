xdumaine/ember-cli

added returnTestState to ember helper to help assert aganst mock-ui and errors

tests/helpers/ember.js
@@ -4,21 +4,27 @@ var MockUI        = require('./mock-ui');
 var MockAnalytics = require('./mock-analytics');
 var Cli           = require('../../lib/cli');
 var path          = require('path');
+var Promise       = require('../../lib/ext/promise');
 
 module.exports = function ember(args, options) {
   var cli;
   var disableDependencyChecker = options && options.disableDependencyChecker || true;
-  var ui = options && options.UI || MockUI;
   var pkg = options && options.package || path.resolve(__dirname, '..', '..');
+  var inputStream  = [];
+  var outputStream = [];
+  var errorStream  = [];
+  var errorLog     = [];
   args.push('--disable-analytics');
   args.push('--watcher=node');
   args.push('--skipGit');
   cli = new Cli({
-    inputStream:  [],
-    outputStream: [],
+    inputStream:  inputStream,
+    outputStream: outputStream,
+    errorStream:  errorStream,
+    errorLog:     errorLog,
     cliArgs:      args,
     Leek: MockAnalytics,
-    UI: ui,
+    UI: MockUI,
     testing: true,
     disableDependencyChecker: disableDependencyChecker,
     cli: {
@@ -28,6 +34,17 @@ module.exports = function ember(args, options) {
       root: pkg 
     }
   });
+  function returnTestState(statusCode) {
+     return {
+        statusCode: statusCode,
+        inputStream: inputStream,
+        outputStream: outputStream,
+        errorStream: errorStream,
+        errorLog: errorLog
+     };
+   }
 
-  return cli;
+  return cli.then(returnTestState, function(statusCode) {
+     return Promise.reject(returnTestState(statusCode));
+  });
 };

mansona/layer-gen

tests/helpers/ember.js
@@ -4,6 +4,7 @@ var MockUI        = require('./mock-ui');
 var MockAnalytics = require('./mock-analytics');
 var cli           = require('../../lib/cli');
 var path          = require('path');
+var Promise       = require('../../lib/ext/promise');
 
 /*
   Accepts a single array argument, that contains the
@@ -43,16 +44,22 @@ module.exports = function ember(args, options) {
   var ui = options && options.UI || MockUI;
   var pkg = options && options.package || path.resolve(__dirname, '..', '..');
   var disableDependencyChecker = options && options.disableDependencyChecker || true;
+  var inputStream  = [];
+  var outputStream = [];
+  var errorStream  = [];
+  var errorLog     = [];
 
   args.push('--disable-analytics');
   args.push('--watcher=node');
   args.push('--skipGit');
   cliInstance = cli({
-    inputStream:  [],
-    outputStream: [],
+    inputStream:  inputStream,
+    outputStream: outputStream,
+    errorStream:  errorStream,
+    errorLog:     errorLog,
     cliArgs:      args,
     Leek: MockAnalytics,
-    UI: ui,
+    UI: MockUI,
     testing: true,
     disableDependencyChecker: disableDependencyChecker,
     cli: {
@@ -62,6 +69,17 @@ module.exports = function ember(args, options) {
       root: pkg 
     }
   });
+  function returnTestState(statusCode) {
+     return {
+        statusCode: statusCode,
+        inputStream: inputStream,
+        outputStream: outputStream,
+        errorStream: errorStream,
+        errorLog: errorLog
+     };
+   }
 
-  return cliInstance;
+  return cliInstance.then(returnTestState, function(statusCode) {
+     return Promise.reject(returnTestState(statusCode));
+  });
 };