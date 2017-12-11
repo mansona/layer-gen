mansona/layer-gen

commit dd1d8d03720051ffb2c7fe4bd93e6b0fab17158c

Fix `ember install` for scoped packages

lib/utilities/get-package-base-name.js
@@ -4,18 +4,9 @@ const npa = require('npm-package-arg');
 
 
 module.exports = function(name) {
-  let parsed, parsedName;
-
   if (!name) {
     return null;
   }
 
-  parsed = npa(name);
-  parsedName = parsed.name;
-
-  if (parsed.scope) {
-    parsedName = parsedName.replace(parsed.scope, '').slice(1);
-  }
-
-  return parsedName;
+  return npa(name).name;
 };

tests/unit/utilities/get-package-base-name-test.js
@@ -8,8 +8,8 @@ describe('getPackageBaseName', function() {
     expect(getPackageBaseName('my-addon')).to.equal('my-addon');
   });
 
-  it('should return the package name without its scope', function() {
-    expect(getPackageBaseName('@scope/my-addon')).to.equal('my-addon');
+  it('should return the full name when scoped', function() {
+    expect(getPackageBaseName('@scope/my-addon')).to.equal('@scope/my-addon');
   });
 
   it('should strip away version numbers', function() {

commit 771750c546db96fa773d69afec920617be46ad12

Fix regression with scoped package name mismatches
https://github.com/ember-cli/ember-cli/commit/dd1d8d03720051ffb2c7fe4bd93e6b0fab17158c introduced a regression where, if your package name is scoped in package.json and not scoped in index.js, `isDevelopingAddon` would now evaluate to `false`, when it used to evaluate to `true`. This caused the `addon` folder to not be linted anymore.

This fixes the regression and allows the old behavior to work again, but also issues a warning, saying you should sync the names up, per the suggestion of @rwjblue

Fix lint issues

Add a test

Use this._hasWarnedForMismatchedNames

Use this.ui.writeWarnLine

Fix tests

tests/unit/models/addon-test.js
@@ -364,7 +364,7 @@ describe('models/addon.js', function() {
 
         project = new Project(projectPath, packageContents, cli.ui, cli);
 
-        addon = new MyAddon(project);
+        addon = new MyAddon(project, project);
 
         originalEnvValue = process.env.EMBER_ADDON_ENV;
       });
@@ -380,6 +380,14 @@ describe('models/addon.js', function() {
       it('returns true when `EMBER_ADDON_ENV` is set to development', function() {
         process.env.EMBER_ADDON_ENV = 'development';
 
+        expect(addon.isDevelopingAddon(), 'addon is being developed').to.eql(true);
+      });
+
+      it('returns true when the addon name is prefixed in package.json and not in index.js', function() {
+        process.env.EMBER_ADDON_ENV = 'development';
+
+        project.name = () => ('@foo/my-addon');
+        addon.name = 'my-addon';
         expect(addon.isDevelopingAddon(), 'addon is being developed').to.eql(true);
       });
 